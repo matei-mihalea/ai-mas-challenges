ac8a34455c04789df75df3606d0b77c5facd8f1e
matei-mihalea <34402356+matei-mihalea@users.noreply.github.com>
2018-10-26 23:58:51 +0300

    Update SignalReceiver.py

--- problem_5_noisy_signal/SignalReceiver.py
+++ problem_5_noisy_signal/SignalReceiver.py
@@ -1,4 +1,7 @@
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import savgol_filter
 
 parser = argparse.ArgumentParser(description='Test number')
 parser.add_argument('test_no', metavar='N', type=int, help='Test number.')
@@ -44,6 +47,10 @@ class SignalReceiver:
 	def get_error(self):
 		return self.__total_error
 
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
 
 if __name__ == "__main__":
 	'''
@@ -52,8 +59,35 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 	sr = SignalReceiver(args.test_no)
 
+
+
+	vals = []
+	index_vals = [0]
+	count = 1
+
 	i_val = sr.get_value()
+	vals.append(i_val)
+
 	while i_val:
-		print(sr.push_value(i_val))
+		ret_val = i_val
+
+		if count > 51:
+			size = count
+			if count % 2 == 0:
+				size = count - 1
+			vals_aprox = savgol_filter(vals, 51, 3) # window size 51, polynomial order 3
+
+			
+			ret_val = (vals_aprox[count-1] + vals[len(vals)-1]) / 2 # medie valoare reala si valoare aproximare
+			
+
+		print(sr.push_value(ret_val))
 		i_val = sr.get_value()
+
+		count += 1
+		vals.append(i_val)
+
+		if count > 100:
+			count -= 1
+			vals.pop(0)
 	print('Total error: ' + str(sr.get_error()))
